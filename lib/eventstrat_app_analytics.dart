9ede6fef20d7eb9639792049c8a4857e91d022d6
basedharsh <basedharsh@gmail.com>
2025-09-01 17:34:03 +0530

    version 1

--- lib/eventstrat_app_analytics.dart
+++ lib/eventstrat_app_analytics.dart
@@ -6,6 +6,8 @@ export 'src/analytics_wrapper.dart';
 // Constants for apps to use
 export 'src/constants/event_actions.dart';
 export 'src/constants/event_category.dart';
+export 'src/constants/event_name.dart';
+export 'src/constants/event_screen_name.dart';
 
 // Router components (will be created later)
 // export 'src/router/analytics_go_route.dart';
