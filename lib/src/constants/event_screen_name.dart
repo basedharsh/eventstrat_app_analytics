5fb217a03460c29fcd3951826e127270ea2aea2a
basedharsh <basedharsh@gmail.com>
2025-09-02 15:20:38 +0530

    version 1

--- lib/src/constants/event_screen_name.dart
+++ lib/src/constants/event_screen_name.dart
@@ -1,81 +1,89 @@
+// lib/constants/analytics_constants.dart
+
+// Events for UPITS Analytics
+
+// Screen names for UPITS Analytics
 class EventScreenName {
-  // ROUTE SCREEN NAMES (auto-generated in screen name also
-  //from Routes)
-  static const v1Splash = 'v1_splash_screen';
-  static const v1AuthOptions = 'v1_auth_options_screen';
-  static const v1Registration = 'v1_registration_screen';
-  static const v1PrivacyPolicy = 'v1_privacy_policy_screen';
-  static const v1Main = 'v1_main_screen';
-  static const v1Home = 'v1_home_screen';
-  static const v1EventUserQr = 'v1_event_user_qr_screen';
-  static const v1EventSpeakers = 'v1_event_speakers_screen';
-  static const v1EventSpeakers2024 = 'v1_event_speakers_2024_screen';
-  static const v1SpeakerDetails = 'v1_speaker_details_screen';
-  static const v1EventAgendaDetails = 'v1_event_agenda_details_screen';
-  static const v1UserProfile = 'v1_user_profile_screen';
-  static const v1ProfilePhotoEdit = 'v1_profile_photo_edit_screen';
-  static const v1ProfileAbout = 'v1_profile_about_screen';
-  static const v1ProfileSocialLinks = 'v1_profile_social_links_screen';
-  static const v1ProfileBasicInfo = 'v1_profile_basic_info_screen';
-  static const v1BookmarkList = 'v1_bookmark_list_screen';
-  static const v1ScannedContacts = 'v1_scanned_contacts_screen';
-  static const v1MeetingsList = 'v1_meetings_list_screen';
-  static const v1EventResources = 'v1_event_resources_screen';
-  static const v1ViewResource = 'v1_view_resource_screen';
-  static const v1FlipBookResource = 'v1_flipbook_resource_screen';
-  static const v1Networking = 'v1_networking_screen';
-  static const v1Community = 'v1_community_screen';
-  static const v1Schedule = 'v1_schedule_screen';
-  static const v1Media = 'v1_media_screen';
-  static const v1ExhibitorsMain = 'v1_exhibitors_main_screen';
-  static const v1ExhibitorsList = 'v1_exhibitors_list_screen';
-  static const v1ExhibitorDetails = 'v1_exhibitor_details_screen';
-  static const v1ExhibitorPdf = 'v1_exhibitor_pdf_screen';
-  static const v1EventGallery = 'v1_event_gallery_screen';
-  static const v1FullScreenImageViewer = 'v1_full_screen_image_viewer_screen';
-  static const v1EventGalleryIndividualGallery =
-      'v1_event_gallery_individual_gallery_screen';
-  static const v1FacePhotoCapture = 'v1_face_photo_capture_screen';
-  static const v1AiChat = 'v1_ai_chat_screen';
-  static const v1PollCreate = 'v1_poll_create_screen';
-  static const v1PostCreate = 'v1_post_create_screen';
-  static const v1Reactions = 'v1_reactions_screen';
-  static const v1Update = 'v1_update_screen';
-  static const v1Maintenance = 'v1_maintenance_screen';
-  static const v1NoInternet = 'v1_no_internet_screen';
-  static const v1Tickets = 'v1_tickets_screen';
-  static const v1CreateTicket = 'v1_create_ticket_screen';
-  static const v1TicketDetails = 'v1_ticket_details_screen';
-  static const v1TicketChat = 'v1_ticket_chat_screen';
-  static const v1ViewProfile = 'v1_view_profile_screen';
-  static const v1ScanQr = 'v1_scan_qr_screen';
-  static const v1AboutEvent = 'v1_about_event_screen';
-  static const v1AboutVenue = 'v1_about_venue_screen';
-  static const v1AboutUttarPradesh = 'v1_about_uttar_pradesh_screen';
-  static const v1ForgotPassword = 'v1_forgot_password_screen';
-  static const v1ForgotPasswordOtp = 'v1_forgot_password_otp_screen';
-  static const v1ResetPassword = 'v1_reset_password_screen';
-  static const v1WebView = 'v1_web_view_screen';
-  static const v1PdfViewer = 'v1_pdf_viewer_screen';
-  static const v1EventAtGlance = 'v1_event_at_glance_screen';
-  static const v1IheFood = 'v1_ihe_food_screen';
-  static const v1FloorPlanDisplay = 'v1_floor_plan_display_screen';
-  static const v1FloorPlanNavigation = 'v1_floor_plan_navigation_screen';
-  static const v1CalendarMain = 'v1_calendar_main_screen';
-  static const v1CalendarCreateSchedule = 'v1_calendar_create_schedule_screen';
-  static const v1CalendarRescheduleMeeting =
-      'v1_calendar_reschedule_meeting_screen';
-  static const v1CalendarAttendeeDetails =
-      'v1_calendar_attendee_details_screen';
-  static const v1CalendarMeetingDetail = 'v1_calendar_meeting_detail_screen';
-  static const v1AdminSendNotification = 'v1_admin_send_notification_screen';
-  static const v1NotificationList = 'v1_notification_list_screen';
-  //
-  static const v1HomeScreen = 'v1_home_screen';
-  static const v1IheFoodScreen = "v1_ihe_food_screen";
-  static const v1NetworkingScreen = "v1_networking_screen";
-  static const v1MeetingScreen = "v1_meetings_screen";
-  static const v1ExhibitorsScreen = "v1_exhibitors_screen";
-  static const v1ProfileScreen = "v1_profile_screen"; //fixed
-  static const v1LoginScreen = "v1_login_screen";
+  // Main app screens
+  static const splash = 'splash_screen';
+  static const home = 'home_screen';
+  static const networking = 'networking_screen';
+  static const exhibitors = 'exhibitors_screen';
+  static const calendar = 'calendar_screen';
+  static const profile = 'profile_screen';
+  static const media = 'media_screen';
+  static const tickets = 'tickets_screen';
+  static const authOtpVerify = 'auth_otp_verify_screen';
+
+  // Auth screens
+  static const authOptions = 'auth_options_screen';
+  static const registration = 'registration_screen';
+  static const privacyPolicy = 'privacy_policy_screen';
+  static const forgotPassword = 'forgot_password_screen';
+  static const forgotPasswordOtp = 'forgot_password_otp_screen';
+  static const resetPassword = 'reset_password_screen';
+
+  // Profile related screens
+  static const bookmarkScreen = 'bookmark_screen';
+  static const scannedContacts = 'scanned_contacts_screen';
+  static const profileEdit = 'profile_edit_screen';
+  static const profilePhotoEdit = 'profile_photo_edit_screen';
+  static const profileAbout = 'profile_about_screen';
+  static const profileSocialLinks = 'profile_social_links_screen';
+  static const profileBasicInfo = 'profile_basic_info_screen';
+
+  // Exhibitor screens
+  static const exhibitorsList = 'exhibitors_list_screen';
+  static const exhibitorDetails = 'exhibitor_details_screen';
+  static const exhibitorPdf = 'exhibitor_pdf_screen';
+
+  // Calendar screens
+  static const calendarMain = 'calendar_main_screen';
+  static const createMeeting = 'create_meeting_screen';
+  static const meetingDetails = 'meeting_details_screen';
+  static const calendarCreateSchedule = 'calendar_create_schedule_screen';
+  static const calendarRescheduleMeeting = 'calendar_reschedule_meeting_screen';
+  static const calendarAttendeeDetails = 'calendar_attendee_details_screen';
+  static const calendarMeetingDetail = 'calendar_meeting_detail_screen';
+
+  // Resource screens
+  static const eventResources = 'event_resources_screen';
+  static const viewResource = 'view_resource_screen';
+  static const flipBookResource = 'flipbook_resource_screen';
+
+  // Lead wallet screens
+  static const viewProfile = 'view_profile_screen';
+  static const leadWallet = 'lead_wallet_screen';
+
+  // Ticket screens
+  static const createTicket = 'create_ticket_screen';
+  static const ticketDetails = 'ticket_details_screen';
+  static const ticketChat = 'ticket_chat_screen';
+
+  // Utility screens
+  static const webView = 'web_view_screen';
+  static const pdfViewer = 'pdf_viewer_screen';
+  static const qrScanner = 'qr_scanner_screen';
+  static const eventUserQr = 'event_user_qr_screen';
+  static const maintenance = 'maintenance_screen';
+  static const update = 'update_screen';
+  static const noInternet = 'no_internet_screen';
+
+  // Notification screens
+  static const notificationList = 'notification_list_screen';
+  static const adminSendNotification = 'admin_send_notification_screen';
+
+  // About screens
+  static const aboutEvent = 'about_event_screen';
+  static const aboutVenue = 'about_venue_screen';
+  static const aboutUttarPradesh = 'about_uttar_pradesh_screen';
+
+  // Event specific screens
+  static const eventAtGlance = 'event_at_glance_screen';
+  static const iheFood = 'ihe_food_screen';
+  static const floorPlanDisplay = 'floor_plan_display_screen';
+  static const floorPlanNavigation = 'floor_plan_navigation_screen';
+
+  // Agenda screens
+  static const eventAgendaDetails = 'event_agenda_details_screen';
 }
